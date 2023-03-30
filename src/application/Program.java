c051126c8e22c0023ab3d6b9d3b629fa24abaa12
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-03-30 14:40:03 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,8 +13,11 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		Seller sel = new Seller(1, "Luiz", "luiz@gmail", new Date(), 3000.0, obj);
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(sel);
+		
+		
 	}
 
 }
